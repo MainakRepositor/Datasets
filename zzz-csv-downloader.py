ed7cb35067a8cef58356793159ef2784daa85801
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2021-12-05 22:06:45 -0800

    Delete zzz-csv-downloader.py

--- zzz-csv-downloader.py
+++
@@ -1,7 +0,0 @@
-import requests
-import pandas as pd
-url = 'https://raw.githubusercontent.com/AbdoSabri99/Face-Recognition-Using-PCA/main/face_data.csv'
-res = requests.get(url, allow_redirects=True)
-with open('face.csv','wb') as file:
-    file.write(res.content)
-face_data = pd.read_csv('face.csv')
